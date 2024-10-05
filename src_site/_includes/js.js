0a6223737be9e25da9a5e8ba55db67e43efc45fb
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2024-10-05 09:58:19 +0300

    oops

--- src_site/_includes/js.js
+++
@@ -1,7 +0,0 @@
-      const currentDate = new Date();
-      const party = [{ m: 10, d: 26 },{ m: 11, d: 28 }];
-
-      for (const date of party) {
-        if (date.m === currentDate.getMonth() && date.d === currentDate.getDay())
-          import('balloons-js').then(m => m.balloons())
-      }
